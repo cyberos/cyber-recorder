a9f761f671507de831762b27aeedf03e39c74ff5
Anonymous <noreply@weblate.org>
2021-02-19 17:43:00 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 0.0% (0 of 9 strings)
    
    Translation: CyberOS/Audio Recorder
    Translate-URL: https://hosted.weblate.org/projects/cyberos/audio-recorder/es_MX/

--- src/translations/es_MX.ts
+++ src/translations/es_MX.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../qml/HomePage.qml" line="231"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cancelar</translation>
     </message>
 </context>
 <context>
