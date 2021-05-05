503df7c0837adbb50423886a68581eeb521855ef
SilvinSooraj <silvanosnelli@gmail.com>
2021-05-05 09:34:57 +0200

    Added translation using Weblate (Malayalam)

---
+++ src/translations/ml_IN.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ml">
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../qml/HomePage.qml" line="40"/>
+        <source>No recordings yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="210"/>
+        <source>Are you sure you want to delete this recording?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="219"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/HomePage.qml" line="231"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecordPage</name>
+    <message>
+        <location filename="../qml/RecordPage.qml" line="108"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/RecordPage.qml" line="124"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/RecordPage.qml" line="135"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Recording</name>
+    <message>
+        <location filename="../recordingmodel.cpp" line="18"/>
+        <source>New Recording</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="16"/>
+        <source>Recorder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
