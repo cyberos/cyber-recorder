ee38bbc4e9453adbf870f2243a1a52c9ceeef940
wolfemium <stefan@grafician.com>
2021-05-06 15:56:36 +0000

    Translated using Weblate (Romanian)
    Currently translated at 88.8% (8 of 9 strings)
    
    Translation: CyberOS/Audio Recorder
    Translate-URL: https://hosted.weblate.org/projects/cyberos/audio-recorder/ro/

--- src/translations/ro_RO.ts
+++ src/translations/ro_RO.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/HomePage.qml" line="40"/>
         <source>No recordings yet</source>
-        <translation type="unfinished">Nicio înregistrare încă</translation>
+        <translation>Nicio înregistrare încă</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="210"/>
         <source>Are you sure you want to delete this recording?</source>
-        <translation type="unfinished">Sunteți sigur că doriți să ștergeți această înregistrare?</translation>
+        <translation>Sunteți sigur că doriți să ștergeți această înregistrare?</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="219"/>
         <source>Delete</source>
-        <translation type="unfinished">Ștergeți</translation>
+        <translation>Ștergeți</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="231"/>
         <source>Cancel</source>
-        <translation type="unfinished">Anulează</translation>
+        <translation>Anulează</translation>
     </message>
 </context>
 <context>
@@ -29,17 +29,17 @@
     <message>
         <location filename="../qml/RecordPage.qml" line="108"/>
         <source>Name</source>
-        <translation type="unfinished">Nume</translation>
+        <translation>Nume</translation>
     </message>
     <message>
         <location filename="../qml/RecordPage.qml" line="124"/>
         <source>Done</source>
-        <translation type="unfinished">Realizat</translation>
+        <translation>Realizat</translation>
     </message>
     <message>
         <location filename="../qml/RecordPage.qml" line="135"/>
         <source>Delete</source>
-        <translation type="unfinished">Ștergeți</translation>
+        <translation>Ștergeți</translation>
     </message>
 </context>
 <context>
@@ -55,7 +55,7 @@
     <message>
         <location filename="../qml/main.qml" line="16"/>
         <source>Recorder</source>
-        <translation type="unfinished"></translation>
+        <translation>Înregistrator</translation>
     </message>
 </context>
 </TS>
