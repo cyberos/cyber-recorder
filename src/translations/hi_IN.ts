05775bf66bfbec89dff609f9af59650a3f9c2e93
SilvinSooraj <silvanosnelli@gmail.com>
2021-05-15 15:11:18 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: CyberOS/Audio Recorder
    Translate-URL: https://hosted.weblate.org/projects/cyberos/audio-recorder/hi/

--- src/translations/hi_IN.ts
+++ src/translations/hi_IN.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/HomePage.qml" line="40"/>
         <source>No recordings yet</source>
-        <translation type="unfinished">अभी तक कोई रिकॉर्डिंग नहीं</translation>
+        <translation>कोई रिकॉर्डिंग नहीं है</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="210"/>
         <source>Are you sure you want to delete this recording?</source>
-        <translation type="unfinished">क्या आप वाकई इस रिकॉर्डिंग को हटाना चाहते हैं?</translation>
+        <translation>क्या आप इस रिकॉर्डिंग को हटाना चाहते हैं?</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="219"/>
         <source>Delete</source>
-        <translation type="unfinished">हटाएं</translation>
+        <translation>हटाएं</translation>
     </message>
     <message>
         <location filename="../qml/HomePage.qml" line="231"/>
         <source>Cancel</source>
-        <translation type="unfinished">रद्द करना</translation>
+        <translation>रद्द करो</translation>
     </message>
 </context>
 <context>
@@ -29,17 +29,17 @@
     <message>
         <location filename="../qml/RecordPage.qml" line="108"/>
         <source>Name</source>
-        <translation type="unfinished">नाम</translation>
+        <translation>नाम</translation>
     </message>
     <message>
         <location filename="../qml/RecordPage.qml" line="124"/>
         <source>Done</source>
-        <translation type="unfinished">किया हुआ</translation>
+        <translation>किया है</translation>
     </message>
     <message>
         <location filename="../qml/RecordPage.qml" line="135"/>
         <source>Delete</source>
-        <translation type="unfinished">हटाएं</translation>
+        <translation>हटाएं</translation>
     </message>
 </context>
 <context>
@@ -47,7 +47,7 @@
     <message>
         <location filename="../recordingmodel.cpp" line="18"/>
         <source>New Recording</source>
-        <translation type="unfinished">नई रिकॉर्डिंग</translation>
+        <translation>नई रिकॉर्डिंग</translation>
     </message>
 </context>
 <context>
@@ -55,7 +55,7 @@
     <message>
         <location filename="../qml/main.qml" line="16"/>
         <source>Recorder</source>
-        <translation type="unfinished">रिकॉर्डर</translation>
+        <translation>रिकॉर्डर</translation>
     </message>
 </context>
 </TS>
